2eef5fe3df2e3ff2466d7588dc7a3cf9c04904b0
Yoel Sanchez Araujo <yoelsanchezaraujo@Yoels-Mac-mini.local>
2021-06-01 00:28:54 -0400

    cont. re-organization

--- julia/generate_simulated_data.jl
+++ julia/generate_simulated_data.jl
@@ -1,11 +1,13 @@
 using Distributions;
+using Distances;
+using KernelFunctions;
 
-function make_xy(x, y, x_mu, y_mu, bx_left, bx_right, by_down, by_up, lr_lr, lr_ud, nsteps)
+function make_xy(x, y, x_mu, y_mu, sigma, bx_left, bx_right, by_down, by_up, lr_lr, lr_ud, nsteps)
     xy = zeros(nsteps, 2)
     for k in 1:nsteps
-        x_sample = rand(Normal(x_mu, 1.))
+        x_sample = rand(Normal(x_mu, sigma))
         x = x + lr_lr * (x_sample - x)
-        y_sample = rand(Normal(y_mu, 1.))
+        y_sample = rand(Normal(y_mu, sigma))
         y = y + lr_ud * (y_sample - y)
         if x <= bx_right && x >= bx_left
             xy[k, 1] = x
@@ -21,4 +23,41 @@ function make_xy(x, y, x_mu, y_mu, bx_left, bx_right, by_down, by_up, lr_lr, lr_
     return xy
 end
 
+sqexpkernel(alpha::Real, rho::Real) = alpha^2 * transform(SqExponentialKernel(), 1/(rho*sqrt(2)))
 
+struct GPSIM
+    X::Array{Float64, 2}
+    y::Array{Int64, 1}
+    f::Array{Float64, 1}
+    K::Array{Float64, 2}
+    rho::Float64
+    alpha::Float64
+end
+
+function sim_data(nsteps, x_bias, y_bias, noise_level)
+    bx_left = -1
+    bx_right = 1
+    by_up = 1
+    by_down = -1
+    nsteps = 1000
+    lr_lr = 0.2
+    lr_ud = 0.2
+    # generate some random x-y positions on a 1 by 1 square
+    X = make_xy(0.6, -0.8,  x_bias, y_bias, noise_level, 
+                bx_left, bx_right, by_down, by_up, lr_lr, lr_ud, nsteps)
+    # parameters for the kernel
+    alpha =rand(LogNormal(0.0, 0.1))
+    rho = rand(LogNormal(1.0, 1.0))
+    kernel = sqexpkernel(alpha, rho)
+    # generate kernel
+    K = kernelmatrix(kernel, XY')
+    m = size(K, 1)
+    sig2 = 1.
+    K = K + I(m) * sig2
+    # and sample a function for the firing rate
+    f = MvNormal(zeros(m), K)
+    f_sampled = rand(f)
+    # now sample spikes from f_sampled
+    y = rand.(Poisson.(exp.(f_sampled)))
+    return GPSIM(X, y, f_sampled, K, rho, alpha)
+end
--- julia/simulations_scratch.jl
+++ julia/simulations_scratch.jl
@@ -2,40 +2,5 @@ base_dir = "/Users/yoelsanchezaraujo/Desktop/gaussian_process_proj/julia/";
 include(joinpath(base_dir, "generate_simulated_data.jl"));
 include(joinpath(base_dir, "gp_inference.jl"));
 
-
-bx_left = -1
-bx_right = 1
-by_up = 1
-by_down = -1
-nsteps = 1000
-lr_lr = 0.2
-lr_ud = 0.2
-x_mu, y_mu = 0.2, 0.7
-
-# generate some random x-y positions on a 1 by 1 square
-X = make_xy(0.6, -0.8,  x_mu, y_mu, bx_left, bx_right, by_down, by_up, lr_lr, lr_ud, nsteps)
-
-# visualize it on that square
-plt.scatter(X[:, 1], X[:, 2], alpha=0.45)
-plt.xlim(-1.1, 1.1)
-plt.ylim(-1.1, 1.1)
-
-
-# now lets create its gram matrix
-sqexpkernel(alpha::Real, rho::Real) = alpha^2 * transform(SqExponentialKernel(), 1/(rho*sqrt(2)))
-
-alpha =rand(LogNormal(0.0, 0.1))
-rho = rand(LogNormal(1.0, 1.0))
-kernel = sqexpkernel(alpha, rho)
-K = kernelmatrix(kernel, XY')
-m = size(K, 1)
-sig2 = 1.
-K = K + I(m) * sig2
-
-# and sample a function for the firing rate
-f = MvNormal(zeros(m), K)
-f_sampled = rand(f)
-
-# now sample spikes from f_sampled
-y = rand.(Poisson.(exp.(f_sampled)))
-
+nsteps, x_bias, y_bias, noise_level = 1000, 0.2, -0.7, 1.
+sim_info = sim_data(nsteps, x_bias, y_bias, noise_level)
